2f11768065fff4c7e28daed114325e9f92b47568
DavideDab <davidepio.dabundo@gmail.com>
2026-06-09 13:30:01 +0200

    -Sistemato boundary di ConsultaPropriePrenotazioni. -Aggiunto boundary di AnnullamentoPopup. -Aggiunto Controller di AnnullaPrenotazione.

---
+++ src/main/java/controller/AnnullaPrenotazioneController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javax.swing.JOptionPane;
+
+public class AnnullaPrenotazioneController {
+
+    // Costruttore o finto Database/Repository per l'esempio
+    public AnnullaPrenotazioneController() {
+        // Qui puoi inizializzare i tuoi oggetti di accesso ai dati (DAO/Repository)
+    }
+    public boolean confermaAnnullamentoPrenotazione() {
+                System.out.println("Prenotazione rimossa dal sistema.");
+                return true;
+    }
+}
